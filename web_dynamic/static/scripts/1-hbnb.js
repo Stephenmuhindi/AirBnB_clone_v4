320a5eb6c51527752a7a011ee571cddc90d66491
Stephen muhindi <bteneson42@gmail.com>
2024-03-03 19:55:11 +0000

    mandatory done

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const selectedAmenities = {};
+  $('input[type="checkbox"][data-id]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+    updateAmenitiesList();
+  });
+
+  function updateAmenitiesList() {
+    const amenitiesArray = Object.values(selectedAmenities);
+    let amenitiesList = amenitiesArray.join(', ');
+    if (amenitiesList.length > 31) {
+      amenitiesList = amenitiesList.slice(0, 31) + '...';
+    }
+
+    $('.amenities h4').text(amenitiesList);
+  }
+});
